e7d178f2147f6d3b28690e91baa1b8be666e1445
Ali Shafahi <ashafahi@gmail.com>
2020-01-02 23:26:12 -0500

    script for downloading cifar10

---
+++ download_cifar10.sh
@@ -0,0 +1,5 @@
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -zxf cifar-10-python.tar.gz
+mv cifar-10-batches-py/* cifar10_data/.
+rm -rf cifar-10-python.tar.gz
+rm -rf cifar-10-python.tar.gz
